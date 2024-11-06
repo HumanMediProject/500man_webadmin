9c93a2f6095e779d97097ffcbd327ada863c8e13
CGH <duck2498@gmail.com>
2024-11-06 17:41:49 +0900

    엑셀 업로드/다운로드 구현

--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,14 @@
 // Call the dataTables jQuery plugin
+
+let table = undefined;
+
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  table = $('#dataTable').DataTable();
 });
+
+const addRow = (data)=>{
+  data.forEach((realrow)=>{
+    table.row.add([realrow["성함"], realrow["주소"], realrow["수당"], realrow["진행 수업"], realrow["연락처"], realrow["계좌정보"]]).draw();
+  });
+}
+
